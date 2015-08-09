dd693ffa6c1f635b0b9dad2af59af3f87f0ea3fc
rockhowse <rockhowse@gmail.com>
2015-08-09 17:39:31 -0400

    got T-006 project compiling before code changes

---
+++ GraphicsTutorials/T-006-VBOSpriteAndNDC/MainGame.cpp
@@ -0,0 +1,98 @@
+#include "MainGame.h"
+#include "MainGame.h"
+
+#include <iostream>
+#include <string>
+
+void fatalError(std::string errorString) {
+	std::cout << errorString << std::endl;
+	std::cout << "Enter any key to quit...";
+	int tmp;
+	std::cin >> tmp;
+}
+
+MainGame::MainGame()
+{
+	_window = nullptr;
+	_screenWidth = 1024;
+	_screenHeight = 768;
+
+	_gameState = GameState::PLAY;
+}
+
+
+MainGame::~MainGame()
+{
+}
+
+void MainGame::run() {
+	initSystems();
+
+	gameLoop();
+}
+
+void MainGame::initSystems() {
+	// Initialize SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	_window = SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
+
+	if (_window == nullptr) {
+		fatalError("SDL Window could not be created.");
+	}
+
+	SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+
+	if (glContext == nullptr) {
+		fatalError("SDL_GL Context could not be created.");
+	}
+
+	GLenum error = glewInit();
+
+	if (error != GLEW_OK) {
+		fatalError("Could not initalize GLEW.");
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+}
+
+void MainGame::gameLoop() {
+	while (_gameState != GameState::EXIT) {
+		processInput();
+		drawGame();
+	}
+}
+
+void MainGame::processInput() {
+	SDL_Event evnt;
+
+	while (SDL_PollEvent(&evnt)){
+		switch (evnt.type){
+			case SDL_QUIT:
+				_gameState = GameState::EXIT;
+				break;
+			case SDL_MOUSEMOTION:
+				std::cout << evnt.motion.x << " " << evnt.motion.y << std::endl;
+				break;
+		}
+	}
+}
+
+void MainGame::drawGame() {
+	glClearDepth(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnableClientState(GL_COLOR_ARRAY);
+	glBegin(GL_TRIANGLES);
+
+	glColor3f(1.0, 0.0, 0.0);
+
+	glVertex2f(0, 0);
+	glVertex2f(0, 500);
+	glVertex2f(500, 500);
+
+	glEnd();
+
+	SDL_GL_SwapWindow(_window);
+}
---
+++ GraphicsTutorials/T-006-VBOSpriteAndNDC/MainGame.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <SDL.h>
+#include <GL/glew.h>
+
+enum class GameState {PLAY, EXIT};
+class MainGame
+{
+public:
+	MainGame();
+	~MainGame();
+
+	void run();
+
+private:
+	SDL_Window* _window;
+	int _screenWidth;
+	int _screenHeight;
+
+	GameState _gameState;
+
+	void initSystems();
+	void gameLoop();
+	void processInput();
+	void drawGame();
+
+};
+
