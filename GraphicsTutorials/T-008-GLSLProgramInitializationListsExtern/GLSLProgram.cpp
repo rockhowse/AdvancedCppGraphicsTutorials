90e4986d1122269177a8a6b31d93284862dbef1f
rockhowse <rockhowse@gmail.com>
2015-08-09 19:32:34 -0400

    refactored original vertex shader compille to generic compileShader() function

--- GraphicsTutorials/T-008-GLSLProgramInitializationListsExtern/GLSLProgram.cpp
+++ GraphicsTutorials/T-008-GLSLProgramInitializationListsExtern/GLSLProgram.cpp
@@ -30,10 +30,19 @@ void GLSLProgram::compileShaders(const std::string& vertexShaderfilePath, std::s
 		fatalError("Fragment shader failed to be created!");
 	}
 
-	std::ifstream vertexFile(vertexShaderfilePath);
+	compileShader(vertexShaderfilePath, _vertexShaderID);
+	compileShader(fragmentShaderFilePath, _fragmentShaderID);
+}
+
+void GLSLProgram::linkShaders() {
+
+}
+
+void GLSLProgram::compileShader(const std::string filePath, GLuint id) {
+	std::ifstream vertexFile(filePath);
 
 	if (vertexFile.fail()) {
-		fatalError("Failed to open: " + vertexShaderfilePath);
+		fatalError("Failed to open: " + filePath);
 	}
 
 	std::string fileContents = "";
@@ -46,34 +55,30 @@ void GLSLProgram::compileShaders(const std::string& vertexShaderfilePath, std::s
 	vertexFile.close();
 
 	const char* contentsPtr = fileContents.c_str();
-	glShaderSource(_vertexShaderID, 1, &contentsPtr, nullptr);
-	glCompileShader(_vertexShaderID);
+	glShaderSource(id, 1, &contentsPtr, nullptr);
+	glCompileShader(id);
 
 	GLint success = 0;
-	glGetShaderiv(_vertexShaderID, GL_COMPILE_STATUS, &success);
+	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
 
 	if (success == GL_FALSE) {
 		GLint maxLength = 0;
-		glGetShaderiv(_vertexShaderID, GL_INFO_LOG_LENGTH, &maxLength);
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The MaxLength includes the NULL character
 		std::vector<char> errorLog(maxLength);
-		glGetShaderInfoLog(_vertexShaderID, maxLength, &maxLength, &errorLog[0]);
+		glGetShaderInfoLog(id, maxLength, &maxLength, &errorLog[0]);
 
 		// Provide the infolog in whatever manor you deem best.
 		// Exit with failure
 
-		glDeleteShader(_vertexShaderID); // don't leak the shader
-		
+		glDeleteShader(id); // don't leak the shader
+
 		std::printf("%s\n",
 			&(errorLog[0]));
 
-		fatalError("Vertex shader failed to compile");
+		fatalError("Shader" + filePath + " failed to compile");
 	}
 
-
 }
 
-void GLSLProgram::linkShaders() {
-
-}
--- GraphicsTutorials/T-008-GLSLProgramInitializationListsExtern/GLSLProgram.h
+++ GraphicsTutorials/T-008-GLSLProgramInitializationListsExtern/GLSLProgram.h
@@ -13,6 +13,8 @@ public:
 	void linkShaders();
 
 private:
+	void GLSLProgram::compileShader(const std::string filePath, GLuint id);
+
 	GLuint _programID;
 
 	GLuint _vertexShaderID;
